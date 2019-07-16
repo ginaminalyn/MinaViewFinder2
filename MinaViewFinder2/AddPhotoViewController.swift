3c9b3ff57ac39bf01bd8c5a7c303871365f8431e
Ginalyn Mina <ginalyn.r.mina@gmail.com>
2019-07-16 09:01:40 -0700

    Iteration 2 complete with photos and text changing as expected

--- MinaViewFinder2/AddPhotoViewController.swift
+++ MinaViewFinder2/AddPhotoViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     var imagePicker = UIImagePickerController()
 
-
     @IBAction func CameraTapped(_ sender: Any) {
         imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)
@@ -41,8 +40,13 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
                 navigationController?.popViewController(animated:true)
         }
     }
-    
-        
+    internal func imagePickerController (_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            newImageView.image = selectedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
@@ -61,4 +65,6 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
     }
     */
 
+
 }
+
