17b82170939f6c023b3309cdf63c415a2a4ffa46
Ginalyn Mina <ginalyn.r.mina@gmail.com>
2019-07-15 18:29:25 -0700

    Iteration 0

---
+++ MinaViewFinder2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MinaViewFinder2
+//
+//  Created by Ginalyn Mina on 7/15/19.
+//  Copyright © 2019 Ginalyn Mina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
